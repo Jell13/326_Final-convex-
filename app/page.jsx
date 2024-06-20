5bba0b15f7c1f7217959ff7824d1057b74b2f03a
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-06-20 12:41:11 -0700

    created the todo front end and using shadcn button

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
+"use client"
 
 export default function Home() {
   return (
-    <div>
-      Testing
+    <div className="h-screen w-screen grainy px-32">
+      <div className="h-full w-full flex flex-col justify-center items-center">
+        <div className="flex flex-col">
+          <h2>Using Convex</h2>
+          <div className="flex gap-3">
+            <input className="rounded-md bg-gray-300" type="text" />
+            <button>Add</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
