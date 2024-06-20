dd1e435c18f3fd628466cce9a6bc5f708c1f96a8
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-06-20 13:50:39 -0700

    added the getter from the database

--- app/page.jsx
+++ app/page.jsx
@@ -18,11 +18,6 @@ export default function Home() {
     setText('')
   }
 
-  useEffect(() => {
-    items
-
-  }, [])
-
   return (
     <div className="h-screen w-screen grainy px-32 overflow-y-scroll">
       <div className="h-full w-full flex flex-col justify-center items-center">
@@ -32,7 +27,11 @@ export default function Home() {
             <input value={text} onChange={(e) => setText(e.target.value)} className="rounded-md py-[2px] px-2 no-outline no-border bg-gray-300" type="text" />
             <button onClick={handleSubmit} className=" py-1 px-2 bg-blue-500 rounded-lg text-white hover:bg-transparent hover:text-blue-500 hover:border-blue-500">Add</button>
           </div>
-          
+          {items?.map(item => (
+            <li key={item}>
+              {item.text}
+            </li>
+          ))}
         </div>
       </div>
     </div>
