f7f1b6143bc73d1e5e9b4b30edc2388c22fc45d5
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-06-24 13:02:05 -0700

    added the speed insights

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ConvexClientProvider from "@/providers/ConvexClientProvider";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <ConvexClientProvider>
           {children}
+          <SpeedInsights />
         </ConvexClientProvider>
       </body>
     </html>
